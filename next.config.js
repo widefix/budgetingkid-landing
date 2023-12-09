0d4173aed32dba4e1053a44655af28fdcd8b872b
Andrei Kaleshka <ka8725@gmail.com>
2023-12-09 17:56:22 +0100

    do not check lint on build

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {};
 if (process.env.NODE_ENV === 'production') {
   nextConfig.output = 'export';
   nextConfig.assetPrefix = 'https://get.budgetingkid.com';
+  nextConfig.eslint = {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  };
 }
 
 module.exports = nextConfig;
