3753c67f6762a5489e406159e433fbe16f8627b5
Andrei Kaleshka <ka8725@gmail.com>
2023-12-09 17:47:05 +0100

    og

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,18 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   metadataBase: new URL('https://get.budgetingkid.com'),
   alternates: { canonical: new URL('https://budgetingkid.com') },
-  title: 'BudgetingKid - Foster good financial habits in your children',
+  title: 'Foster good financial habits in your kids',
   description:
     'Empower your kids with financial literacy and effortlessly manage their allowances. Sign up for our free app today and start fostering a financially savvy future for your children.',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://budgetingkid.com',
+    siteName: 'BudgetingKid',
+    description:
+      'Empower your kids with financial literacy and effortlessly manage their allowances. Sign up for our free app today and start fostering a financially savvy future for your children.',
+    title: 'Foster good financial habits in your kids',
+  }
 };
 
 export default function RootLayout({
