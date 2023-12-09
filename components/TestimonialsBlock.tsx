5d425fd660a4bb6e17da7a08d2bba4a65fb79d8e
Andrei Kaleshka <ka8725@gmail.com>
2023-12-09 18:38:21 +0100

    testimonials

--- components/TestimonialsBlock.tsx
+++ components/TestimonialsBlock.tsx
@@ -8,27 +8,26 @@ import { PropsWithChildren, useRef, useState } from 'react';
 import classNames from 'classnames';
 import Image from 'next/image';
 
-import mordaImg from '@/public/images/morda.png';
-
 const TP: React.FC<PropsWithChildren> = ({ children }) => (
   <p className={'text-[20px] leading-7 text-[#33273D]'}>{children}</p>
 );
 
-const Testimonial: React.FC = () => (
+const Testimonial: React.FC<{
+  name: string;
+  title: string;
+  body: string;
+  img: any;
+}> = ({ name, title, body, img }) => (
   <div className={'p-[20px]'}>
     <blockquote className={'text-[20px] leading-7 text-[#33273D] mb-6'}>
-      &quot;At Boosted Commerce, we are in the business of scaling Shopify
-      stores efficiently. We use Nostra to run sophisticated real time
-      personalization that significantly increases our conversion rate.&quot;
+      &quot;{body}&quot;
     </blockquote>
     <div className="flex items-center">
-      <Image src={mordaImg} alt={'Juan Paul Morda'} />
+      <Image src={img} alt={name} style={{ borderRadius: '50%' }} />
       <div className={'ml-6 flex items-center'}>
-        <h5 className={'font-bold leading-7 text-[#33273D]'}>
-          Juan Paul Morda
-        </h5>
+        <h5 className={'font-bold leading-7 text-[#33273D]'}>{name}</h5>
         <div className={'w-[1px] mx-2 h-6 bg-[#DBD2E3]'}></div>
-        <p className={'leading-7 text-[#33273D]'}>CTO Boosted Commerce</p>
+        <p className={'leading-7 text-[#33273D]'}>{title}</p>
       </div>
     </div>
   </div>
@@ -86,16 +85,36 @@ export default function TestimonialsBlock() {
           autoplay={{ delay: 5000 }}
         >
           <SwiperSlide>
-            <Testimonial />
+            <Testimonial
+              title="Washington, PA - United States"
+              body="It's some what use full, It have good value for families and for childrens to manage the usage of money in digital way also can share each athers"
+              name="Rickea Smith"
+              img={require('@/public/images/ricke_s.jpeg')}
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <Testimonial />
+            <Testimonial
+              title="Goodyear, AZ - United States"
+              body="It is a good introduction to money management"
+              name="Brenda Becker"
+              img={require('@/public/images/brenda_b.jpeg')}
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <Testimonial />
+            <Testimonial
+              title="Phoenix, AZ - United States"
+              body="I find this useful as it gives my kids financial responsibility and also teaches about savings and goals. I love it!"
+              name="Sindhuja M"
+              img={require('@/public/images/sindhupec.jpeg')}
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <Testimonial />
+            <Testimonial
+              title="Freiburg, Baden-Württemberg - Germany"
+              body="I thoroughly enjoyed the straightforwardness, simplicity and ease of use of the product and I would recommend resisting the urge to add more and more features and keep focusing on the essentials."
+              name="Sam Harris"
+              img={require('@/public/images/ricke_s.jpeg')}
+            />
           </SwiperSlide>
         </Swiper>
       </div>
